abad3e86666dd27c4e6a1bf97eca3779d730078e
taterbase <shankga@gmail.com>
2014-11-24 18:43:13 -0700

    Initial Commit

---
+++ EasyJSON/EasyJSON.h
@@ -0,0 +1,19 @@
+//
+//  EasyJSON.h
+//  EasyJSON
+//
+//  Created by George Shank on 11/24/14.
+//  Copyright (c) 2014 Meshable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyJSON.
+FOUNDATION_EXPORT double EasyJSONVersionNumber;
+
+//! Project version string for EasyJSON.
+FOUNDATION_EXPORT const unsigned char EasyJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyJSON/PublicHeader.h>
+
+
